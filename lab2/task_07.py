12798f6f879407b60631f8a46522f869f29d7f2d
phystech-aa <54894234+phystech-aa@users.noreply.github.com>
2019-09-11 22:27:37 +0300

    Update task_07.py

--- lab2/task_07.py
+++ lab2/task_07.py
@@ -1,14 +1,13 @@
-
 import math
 import turtle
 t = turtle.Turtle()
 
-k =5
+k = 5
 delta_l = 8
 phi = 0
 for i in range(400):
     r = k*phi 
     delta_phi = delta_l / (k+r)
-    t.left(180*delta_phi / math.pi)
+    t.left(180*delta_phi/math.pi)
     phi += delta_phi
     t.forward(delta_l)
